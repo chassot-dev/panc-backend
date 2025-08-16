1f7b76764bf478f7cd78711efed3daa5c90dc401
Harrison Guinter Zimmer Dietze <harrison.dietze@campus.labs>
2025-08-16 04:19:00 -0300

    Alteração de retorno da detecção

--- src/rest/routes/panc.routes.ts
+++ src/rest/routes/panc.routes.ts
@@ -8,9 +8,7 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 router.get('/',  (req, res, next) => pancController.getAll(req, res, next));
-router.post('/detect', upload.single('image'), (req, res) => { 
-	console.log(req.file); 
-	pancController.detect(req, res) })
+router.post('/detect', upload.single('image'), (req, res) => { pancController.detect(req, res) })
 router.get('/:id', (req, res, next) => pancController.findById(req, res, next));
 router.patch('/update/:id', (req, res, next) => pancController.updateInfo(req, res, next));
 
